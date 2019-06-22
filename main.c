697b3c1f2da443f40c31b8b8e87b0f8789d8fad1
Tamir <1tamir198@gmail.com>
2019-06-22 20:41:12 +0300

    Learning cool tricks with character functions (isupper,isaplha etc...)

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 #include <stdio.h> //standard  input output
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
 #include "tamirInfo.h"
 
 
@@ -17,12 +20,70 @@ int main()
 {
 
 
-    printf(AGE);
 
 
 
 
-    /*
+    /**************************************************************************/
+
+    /*check if the letter is capital or not
+
+    int isUpperCase = 'd';
+
+    if ( isalpha(isUpperCase) ){
+        if(isupper(isUpperCase)){
+            printf(" %c is capital",isUpperCase);
+        }else{
+            printf(" %c is not capital",isUpperCase);
+        }
+    }
+
+    */
+
+
+    /**************************************************************************/
+
+    /*In c this is how you check is int/char are numbers or chars
+
+    int number = '9';
+
+    if ( isalpha(number) ){
+            printf(" %c this is a number",number);
+    }else if (isdigit(number)){
+            printf(" %c this is a char",number);
+    }
+    */
+
+    /**************************************************************************/
+
+    /* This is how you check is a letter is from the abc
+    int some = "a";
+
+    if(!isalpha(some)){
+        printf("it is abd");
+
+    }else{
+        printf("you dumb shit");
+    }
+
+    */
+
+
+
+    /**************************************************************************/
+
+
+    /*sHORTEN IF IN C
+
+    (1 ==2) ? printf("Condition is true") : printf("Condition is false");
+
+
+
+
+
+    /**************************************************************************/
+
+    /*Replacing the array context
     char myName [] = "tamir" ;
     //In c this is how you replace something that inside array
     strcpy(myName,"lalal");
@@ -30,5 +91,6 @@ int main()
     */
 
 
+    /**************************************************************************/
     return 0;
 }
