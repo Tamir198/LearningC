4e593e28d5b96ba5bea3f7cf698e6f43702985f7
Tamir <1tamir198@gmail.com>
2019-06-25 21:24:16 +0300

    Learning about pointers and strings limit
    1) Limit the max size of string accepted as input from the user.
    2) Introduction to pointers 
    3) Arrays and pointers

--- main.c
+++ main.c
@@ -19,31 +19,97 @@
 
 int main()
 {
+
+    /**************************************************************************/
+
+    ///string and  lengths of them:
+
+    char someString [20];
+    char * pSomeString = someString;
+
+    fgets(pSomeString,20,stdin);
+    //fgets(where to store it,max chars,input type stdin for key board);
+    puts(pSomeString);
+    /**************************************************************************/
+
+
+    ///Pointers intro:
+
+    /*
+
+    int age = 9;
+
+    //%p for pointer I want to print the memory address
+    //& - to go when your something is - &age -> go to where your age is saved on your memory.
+    printf("%p \n",&age );
+
+    //creating pointer:
+    int * pAge = &age;
+    printf("%p this \n",pAge);//prints the memory address
+
+    //de-referenc a pointer
+
+    //go to the variable that pAge points to and gets the value of it
+    printf("*pAge value is %d \n",*pAge);
+
+    */
+
+    ///arrays and pointers aka: "there is no god at this point(er)"
+
+
+    /*
+    int array [5] = {56,48,321,4,126};
+    int i;
+
+    for(i=0 ; i<5 ; i++){
+        printf("in \t array[%d] \t the memory address  %p \t the value %d \n" ,i,&array[i],array[i]);
+    }
+
+     /// with arrays you don`t have to use & before :
+     ///it will just give you pointer to the first element of the array
+
+     printf("check my array pointer %p \n",array);
+
+
+     printf("check my dereferenced array pointer %d \n",*array);
+
+
+     */
+
+
+     /*
+     ///You can NOT change the array like this ->[string = "some"] because the name of the array is constant
+     ///BUT you can change the value of it with pointers
+     char string [] = "some thing is real";
+     puts(string);
+     char * pString = "this is pointer to array";
+     puts(pString);
+     pString = "look how this value is changing";
+     puts(pString);
+
+     */
+
+
+
     /**************************************************************************/
     ///Messing with math
+    /*
 
-    //absolute  value abs()
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
-    printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
     printf("Dice roll 1-6... %d \n" , (rand()%6) + 1 ); //generates random number with custom range 1-6
     printf("Rundom number... %d \n" , rand()); //generates random number
 
 
     printf("%f is the square root of 64 ", sqrt(64));
     printf("%f is the hezka (5^3 = 5*5*5) of 5^3 %d \n",pow(5,3));
+    //absolute  value abs()
     printf("this is absolute value of %d \n",abs(3-9));
 
 
     float someFloat = 5.4564;
     printf("%.2f rounding to bottom is \n",floor(someFloat));
     printf("%.2f rounding up is \n",ceil(someFloat));
+
+    */
 
     /**************************************************************************/
     ///gets and puts
